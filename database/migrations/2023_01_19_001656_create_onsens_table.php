326ca4ca8983242604ceedb397490fe19d519d97
Sarami0202 <sarami0202@icloud.com>
2023-02-04 20:49:38 +0900

    category変更

--- database/migrations/2023_01_19_001656_create_onsens_table.php
+++ database/migrations/2023_01_19_001656_create_onsens_table.php
@@ -19,7 +19,7 @@ return new class extends Migration {
             $table->string('prefecture', 255);
             $table->string('area', 255);
             $table->string('address', 255);
-            $table->string('category', 255);
+            $table->integer('category');
             $table->string('body', 255);
             $table->string('info', 255)->nullable();
             $table->string('instagram', 255)->nullable();
