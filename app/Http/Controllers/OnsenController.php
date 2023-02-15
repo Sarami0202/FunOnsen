56a61785370d978870abe66ddd7ea79f6379616f
Sarami0202 <sarami0202@icloud.com>
2023-02-15 19:20:18 +0900

    2023/02/15

--- app/Http/Controllers/OnsenController.php
+++ app/Http/Controllers/OnsenController.php
@@ -25,52 +25,58 @@ class OnsenController extends Controller
      */
     public function store(Request $request)
     {
-        $d_path = array(null, null, null, null, null, );
-        if ($request['thumbnail'] != null) {
-            $thumbnail_file = $request->file('thumbnail');
-            foreach ($thumbnail_file as $index => $img) {
-                $d_path[$index] = isset($img) ? $img->store('thumbnail', 'public') : null;
-            }
-        }
-        $m_path = null;
-        if ($request['main'] != null) {
-            $main_file = $request->file('main');
-            $m_path = isset($main_file) ? $main_file->store('thumbnail', 'public') : null;
-        }
 
-        onsen::insert([
-            'name' => $request->name,
-            'region' => $request->region,
-            'prefecture' => $request->prefecture,
-            'area' => $request->area,
-            'address' => $request->address,
-            'category' => $request->category,
-            'categoryname' => $request->categoryname,
-            'body' => $request->body,
-            'html' => $request->html,
-            'instagram' => $request->instagram,
-            'twitter' => $request->twitter,
-            'facebook' => $request->facebook,
-            'youtube' => $request->youtube,
-            'google' => $request->google,
-            'tiktok' => $request->tiktok,
-            'info1' => $request->info1,
-            'label1' => $request->label1,
-            'info2' => $request->info2,
-            'label2' => $request->label2,
-            'info3' => $request->info3,
-            'label3' => $request->label3,
-            'info4' => $request->info4,
-            'label4' => $request->label4,
-            'info5' => $request->info5,
-            'label5' => $request->label5,
-            'thumbnail1' => $m_path,
-            'thumbnail2' => $d_path[0],
-            'thumbnail3' => $d_path[1],
-            'thumbnail4' => $d_path[2],
-            'thumbnail5' => $d_path[3],
-            'thumbnail6' => $d_path[4],
-        ]);
+        $check = onsen::where('name', $request[`name`])->get();
+        if (count($check) == 0) {
+            $d_path = array(null, null, null, null, null, );
+            if ($request['thumbnail'] != null) {
+                $thumbnail_file = $request->file('thumbnail');
+                foreach ($thumbnail_file as $index => $img) {
+                    $d_path[$index] = isset($img) ? $img->store('thumbnail', 'public') : null;
+                }
+            }
+            $m_path = null;
+            if ($request['main'] != null) {
+                $main_file = $request->file('main');
+                $m_path = isset($main_file) ? $main_file->store('thumbnail', 'public') : null;
+            }
+
+            onsen::insert([
+                'name' => $request->name,
+                'region' => $request->region,
+                'prefecture' => $request->prefecture,
+                'area' => $request->area,
+                'address' => $request->address,
+                'category' => $request->category,
+                'categoryname' => $request->categoryname,
+                'body' => $request->body,
+                'html' => $request->html,
+                'instagram' => $request->instagram,
+                'twitter' => $request->twitter,
+                'facebook' => $request->facebook,
+                'youtube' => $request->youtube,
+                'google' => $request->google,
+                'tiktok' => $request->tiktok,
+                'info1' => $request->info1,
+                'label1' => $request->label1,
+                'info2' => $request->info2,
+                'label2' => $request->label2,
+                'info3' => $request->info3,
+                'label3' => $request->label3,
+                'info4' => $request->info4,
+                'label4' => $request->label4,
+                'info5' => $request->info5,
+                'label5' => $request->label5,
+                'thumbnail1' => $m_path,
+                'thumbnail2' => $d_path[0],
+                'thumbnail3' => $d_path[1],
+                'thumbnail4' => $d_path[2],
+                'thumbnail5' => $d_path[3],
+                'thumbnail6' => $d_path[4],
+            ]);
+            return (1);
+        } else
+            return (0);
 
     }
 
